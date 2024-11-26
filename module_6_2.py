bb8e29b33c6f13645f43d1f995e2e3062aabdb62
Niko <75286064+Amphetam1ne@users.noreply.github.com>
2024-11-26 21:17:08 +0200

    Update module_6_2.py

--- module_6_2.py
+++ module_6_2.py
@@ -34,7 +34,7 @@ class Sedan(Vehicle):
 
 
 # Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
-vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 500, 'blue')
 
 # Изначальные свойства
 vehicle1.print_info()
